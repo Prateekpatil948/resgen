4675bcb72879c54b61732126882f42f795243354
Prateekpatil948 <159271568+Prateekpatil948@users.noreply.github.com>
2025-06-10 00:02:18 +0530

    Update admin.py
    admin panel changed

--- pdf/admin.py
+++ pdf/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 # Register your models here.
+admin.site.site_header = "ResGen"
+admin.site.site_title = " ResGen Resume Generator "
+admin.site.index_title = "Resgen Administor"
+
 admin.site.register(Profile)
